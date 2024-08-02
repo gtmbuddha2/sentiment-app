92991734b80e927c6cb2de4f415dbdb39694b523
gautambuddha <nimesgautam@gmail.com>
2024-08-02 15:54:56 +0545

    database: connected to neon database

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+
+const isPublicRoute = createRouteMatcher([
+  '/',
+]);
+
+export default clerkMiddleware((auth, req) => {
+
+  if (isProtectedRoute(req)) auth().protect();
+
+});
 
 export const config = {
   matcher: [
